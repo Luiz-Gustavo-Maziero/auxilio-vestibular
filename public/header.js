4e5de82cca21dbc02d587076f31f9535617a44bd
Marcus Vinicius <luizgustavosm03@gmail.com>
2025-05-04 21:05:45 -0300

    teste no header

--- public/header.js
+++ public/header.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("../views/header.html",  { cache: "no-store" })
+    fetch("tcc/views/header.html",  { cache: "no-store" })
       .then(res => res.text())
       .then(data => {
         document.getElementById("header").innerHTML = data;
